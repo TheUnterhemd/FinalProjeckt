5537b33af083bc5fb3ac8c4cdff64fa64cf3745c
TheUnterhemd <kodel.thomas83@gmail.com>
2023-05-03 16:07:03 +0200

    small server and Dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config()
+const app = express();
+
+
+
+
+app.listen(process.env.PORT, () =>{
+    console.log("Lauschangriff auf Port:"+ process.env.PORT);
+})
