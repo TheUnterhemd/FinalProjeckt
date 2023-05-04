9eb396c13e058d7e610f5ba375371b6182c51b19
TheUnterhemd <kodel.thomas83@gmail.com>
2023-05-04 14:20:18 +0200

    simple trainer router model and controller; DBcon

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import trainerRouter from './routes/trainerRouter.js';
 
 dotenv.config()
 const app = express();
 
+mongoose.connect(`mongodb+srv://${process.env.MDB_USER}:${process.env.MDB_PASS}@localtrainer.zdzuwkb.mongodb.net/?retryWrites=true&w=majority`)
 
+app.use("/trainer", trainerRouter)
 
 
 app.listen(process.env.PORT, () =>{
