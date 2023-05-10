79d15980ec7a5464b4a028d6fdb35a9e43bbb5fb
dkathrine <101878099+dkathrine@users.noreply.github.com>
2023-05-10 10:18:52 +0200

    added ThemeContext.js

--- client/src/context/ThemeContext.js
+++ client/src/context/ThemeContext.js
@@ -1,23 +1,58 @@
-// color theme example from Material
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createContext, useState, useMemo } from "react";
+import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: "#757ce8",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000",
-    },
-  },
-});
 
-export const Themeprov = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
+export const ColorModeContext = createContext({toggleColorMode: () => {} });
+
+export default function ToggleColorMode({children}) {
+    const [mode, setMode] = useState('dark');
+    const colorMode = useMemo(
+      () => ({
+        toggleColorMode: () => {
+          setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
+        },
+      }),
+      [],
+    );
+  
+    let theme = useMemo(
+      () =>
+        createTheme({
+          palette: {
+            mode,
+            ...(mode === 'dark' 
+            ? {
+              primary: {
+                main: "#f5f5f5",
+              },
+              secondary: {
+                main: "#ffffff",
+              },
+              text: {
+                primary: "#f5f5f5",
+                secondary: "#EBCBE5",
+              },
+              background: {
+                default: "#0a1d30",
+                paper: "#276fc1",
+              },
+            } : {
+              primary: {
+                main: "#00FF09"
+              },
+            })
+          },
+        }),
+      [mode],
+    );
+
+    theme = responsiveFontSizes(theme);
+  
+    return (
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          {children}
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    );
+  }
--- client/src/index.js
+++ client/src/index.js
@@ -4,42 +4,16 @@ import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import CssBaseline from "@mui/material/CssBaseline";
-// color theme example from Material
-import {
-  createTheme,
-  responsiveFontSizes,
-  ThemeProvider,
-} from "@mui/material/styles";
-
-//colorThemes
-let theme = createTheme({
-  palette: {
-    primary: {
-      main: "#f5f5f5",
-    },
-    secondary: {
-      main: "#ffffff",
-    },
-    text: {
-      primary: "#f5f5f5",
-      secondary: "#EBCBE5",
-    },
-    background: {
-      default: "#0a1d30",
-      paper: "#276fc1",
-    },
-  },
-});
-theme = responsiveFontSizes(theme);
+import ToggleColorMode from "./context/ThemeContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ToggleColorMode>
       <CssBaseline />
       <AuthProvider>
         <App />
       </AuthProvider>
-    </ThemeProvider>
+    </ToggleColorMode>
   </React.StrictMode>
 );
