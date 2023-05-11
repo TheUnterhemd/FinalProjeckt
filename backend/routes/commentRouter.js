1c2fd97e65dff2aada037a31226050139fb45e33
TheUnterhemd <kodel.thomas83@gmail.com>
2023-05-11 14:52:29 +0200

    imports added

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-//import {addCourse,updateCourse,getAllCourses,getCourse,deleteCourse} from "../controller/courseController.js"
+import {addComment,updateComment,getAllComments,deleteComment} from "../controller/commentController.js"
 
 const commentRouter = express.Router();
 
