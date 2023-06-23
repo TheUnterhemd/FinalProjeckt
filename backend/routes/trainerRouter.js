d17b19e2b7da9f92331ddcea04851ee442f8327a
Michael Lieber <m.lieber@posteo.de>
2023-06-23 11:19:38 +0200

    changed multer of trainer profile update

--- backend/routes/trainerRouter.js
+++ backend/routes/trainerRouter.js
@@ -7,23 +7,27 @@ import {
   updateTrainer,
   logoutTrainer,
   passwordChange,
-  emailChange
+  emailChange,
 } from "../controller/trainerController.js";
 import { upload } from "../controller/cloudinaryController.js";
 import { trainerValidator } from "../validation/validator.js";
-
 
 const trainerRouter = express.Router();
 
 trainerRouter.post("/register", upload.single("imgURL"), addTrainer);
 trainerRouter.post("/login", loginTrainer);
-trainerRouter.post("/logout", logoutTrainer); 
+trainerRouter.post("/logout", logoutTrainer);
 
-trainerRouter.put("/password/:id", trainerValidator, passwordChange)
-trainerRouter.put("/email/:id", trainerValidator, emailChange)
-trainerRouter.put("/update/:id",trainerValidator, upload.single("imageURL"), updateTrainer);
+trainerRouter.put("/password/:id", trainerValidator, passwordChange);
+trainerRouter.put("/email/:id", trainerValidator, emailChange);
+trainerRouter.put(
+  "/update/:id",
+  trainerValidator,
+  upload.single("imgURL"),
+  updateTrainer
+);
 
-trainerRouter.get("/",getAllTrainers);
+trainerRouter.get("/", getAllTrainers);
 trainerRouter.get("/:id", getTrainer);
 
 export default trainerRouter;
